969682000bc9191c8e9f7150479bf8e7de3cc86d
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2024-08-29 22:08:00 -0300

    Cria Pedido.java em domain

---
+++ estudoDirigido/estudo_dirigido/src/main/java/iftm/edu/br/dnolavo/estudo_dirigido/domain/Pedido.java
@@ -0,0 +1,24 @@
+package iftm.edu.br.dnolavo.estudo_dirigido.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPedido;
+    private Long idNf;
+    private Long idItem;
+    private Long codProd;
+    private Double valorUnit;
+    private Long quantidade;
+    private Long desconto;
+}
+
