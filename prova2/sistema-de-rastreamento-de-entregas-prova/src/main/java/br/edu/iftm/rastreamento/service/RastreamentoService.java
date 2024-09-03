807eaffac1d00f6ee86bd7f707dff6b09fb58688
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2024-09-02 22:35:07 -0300

    Atualização de "RastreamentoService.java"

--- prova2/sistema-de-rastreamento-de-entregas-prova/src/main/java/br/edu/iftm/rastreamento/service/RastreamentoService.java
+++ prova2/sistema-de-rastreamento-de-entregas-prova/src/main/java/br/edu/iftm/rastreamento/service/RastreamentoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.edu.iftm.rastreamento.model.Pacote;
 import br.edu.iftm.rastreamento.model.Rastreamento;
+import br.edu.iftm.rastreamento.service.exceptions.NaoAcheiException;
 
 @Service
 public class RastreamentoService {
@@ -16,7 +17,9 @@ public class RastreamentoService {
 
     public List<Rastreamento> getRastreamentos(Long id) {
         Pacote pacote = pacoteService.getPacoteById(id);
+        if (pacote.getRastreamentos().isEmpty()) {
+            throw new NaoAcheiException("Nenhum rastreamento encontrado para o pacote com ID " + id);
+        }
         return pacote.getRastreamentos();
     }
-
 }
