a7e95e837c92c94dda378ee4833ce1e88859d922
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2024-09-02 22:33:54 -0300

    Atualização de "ExceptionsController.java"

--- prova2/sistema-de-rastreamento-de-entregas-prova/src/main/java/br/edu/iftm/rastreamento/controller/ExceptionsController.java
+++ prova2/sistema-de-rastreamento-de-entregas-prova/src/main/java/br/edu/iftm/rastreamento/controller/ExceptionsController.java
@@ -2,16 +2,21 @@ package br.edu.iftm.rastreamento.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.*;
 
 import br.edu.iftm.rastreamento.service.exceptions.NaoAcheiException;
 
 @ControllerAdvice
 public class ExceptionsController {
 
-	@ExceptionHandler(NaoAcheiException.class)
-	public ResponseEntity<?> naoAchei(NaoAcheiException e) {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-	}
+    @ExceptionHandler(NaoAcheiException.class)
+    public ResponseEntity<String> handleNaoAcheiException(NaoAcheiException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Ocorreu um erro interno no servidor. Por favor, tente novamente mais tarde.");
+    }
 }
